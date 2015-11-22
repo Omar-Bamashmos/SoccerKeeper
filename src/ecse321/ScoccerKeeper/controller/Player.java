a52af90ea0878ce419e14667a49018ee7cd1bf0b
Omar-Bamashmos <omar.bamashmos1994@gmail.com>
2015-11-22 13:09:22 -0500

    Update Player.java

--- src/ecse321/ScoccerKeeper/controller/Player.java
+++ src/ecse321/ScoccerKeeper/controller/Player.java
@@ -3,9 +3,16 @@ package ecse321.ScoccerKeeper.controller;
 import java.awt.List;
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Omar
+ * 
+ * This class stores the data of the player object
+ *
+ */
 public class Player {
-	private static String Name;
-	private static int jerseynumber;
+	private  String Name;
+	private int jerseynumber;
 	private static String Nationality;
 	private ArrayList<Shot> shots= new ArrayList<>();
 	private ArrayList <Infraction>infractions= new ArrayList<>();
@@ -15,28 +22,79 @@ public class Player {
 		this.jerseynumber=JN;
 		this.Nationality=nationality;
 	}
-	
+	/**
+	 * 
+	 * @return player's Name
+	 */
 	public String getName() {
 		return Name;
 	}
+	
+	/**
+	 * 
+	 * @return player's jersey number
+	 */
 	public int getJN() {
 		return jerseynumber;
 	}
-	
+	/**
+	 * 
+	 * @param shot
+	 * add a shot to the player record
+	 */
 	public void addShot(Shot shot) {
 		shots.add(shot);
 	}
-	
+	/**
+	 * 
+	 * @param infraction
+	 * add infraction to the player record
+	 */
 	public void addInfraction(Infraction infraction) {
 		infractions.add(infraction);
 		
 	}
-	public ArrayList getShots(){
+	/**
+	 * 
+	 * @return list of shots
+	 */
+	public ArrayList<Shot> getShots(){
 		return shots;
 	}
-	public ArrayList getInfractions() {
+	/**
+	 * 
+	 * @return list of infractions
+	 */
+	public ArrayList<Infraction> getInfractions() {
 		return infractions;
 	}
+	/**
+	 * 
+	 * @return number of goals
+	 * 
+	 */
+	public int getNumOfGoals() {
+		int numOfGoals=0;
+		if(shots==null)
+			return 0;
+		else {
+		for(Shot shot:shots) {
+			if(shot.equals(Shot.Goal))
+				numOfGoals++;
+		}
+		}
+		return numOfGoals;	
+			
+	}
+	/**
+	 * 
+	 * @return number of infractions in the player record
+	 */
+	public int getNumOfInfractions() {
+		if (infractions==null)
+			return 0;
+		else
+			return this.infractions.size();
+	}
 	
-
 }
