28a94a71ecb529118a670f59650db3079c71e110
Omar-Bamashmos <omar.bamashmos1994@gmail.com>
2015-11-22 13:07:13 -0500

    Update gameKeeper.java

--- src/ecse321/ScoccerKeeper/controller/gameKeeper.java
+++ src/ecse321/ScoccerKeeper/controller/gameKeeper.java
@@ -1,7 +1,9 @@
 package ecse321.ScoccerKeeper.controller;
 
 public interface gameKeeper {
-	public  void addgame(Game game);
-	public  void addShot(Shot shot);
-	public void addInfraction(Infraction infraction);
+	//public  void addgame(Game game);
+	public  void addShots(Shot shot, Player player, Team team);
+	public void addInfractions(Infraction infraction, Player player);
+	public int[] gameResults();
+	public int teamOneGoals();
 }
