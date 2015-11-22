1594a7ae3852a18aa0a2c8a45fcb4324ed94ccec
Omar-Bamashmos <omar.bamashmos1994@gmail.com>
2015-11-22 13:16:12 -0500

    Update season.java

--- src/ecse321/ScoccerKeeper/controller/season.java
+++ src/ecse321/ScoccerKeeper/controller/season.java
@@ -2,20 +2,28 @@ package ecse321.ScoccerKeeper.controller;
 
 import java.util.ArrayList;
 
-public class season {
+/**
+ * 
+ * @author Omar
+ * Stores the data of the leagues in the season
+ */
+public class Season {
 	
 	private static String SeasonName;
-	private ArrayList<league> leagues=new ArrayList<>();
+	private ArrayList<League> leagues=new ArrayList<>();
 	
-	public season (String seasonName) {
+	public Season (String seasonName) {
 		this.SeasonName=seasonName;
 	}
 	
-	public void addLeague (league league) {
+	public void addLeague (League league) {
 		leagues.add(league);
 	}
+	public String getName() {
+		return SeasonName;
+	}
 	
-	public ArrayList getLeagues() {
+	public ArrayList<League> getLeagues() {
 		return leagues;
 	}
 }
