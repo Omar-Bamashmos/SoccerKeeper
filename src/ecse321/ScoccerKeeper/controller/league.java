552e945cd7efc3a62e28f1f58f8db0140792f338
Omar-Bamashmos <omar.bamashmos1994@gmail.com>
2015-11-22 13:14:04 -0500

    Update league.java

--- src/ecse321/ScoccerKeeper/controller/league.java
+++ src/ecse321/ScoccerKeeper/controller/league.java
@@ -2,22 +2,48 @@ package ecse321.ScoccerKeeper.controller;
 
 import java.util.ArrayList;
 
-public class league {
-	private static String leagueName;
+/**
+ * 
+ * @author Omar
+ * 
+ * This class is a component class that stores the data of the league
+ * it has the list of the team playing in the league
+ *
+ */
+
+public class League {
+	private  String leagueName;
 	private ArrayList<Team> teams=new ArrayList<>();
-	private ArrayList<Game> games=new ArrayList<>();
 	
-	public league (String leaguename) {
+	
+	/**
+	 * 
+	 * @param leaguename
+	 * Create a new league is only allowed to the manager mode
+	 */
+	public League (String leaguename) {
 		this.leagueName=leaguename;
 	}
-	
+	/**
+	 * 
+	 * @return league name
+	 */
 	public String getLeagueName () {
 		return leagueName;
 	}
+	/**
+	 * 
+	 * @param team
+	 * adding teams to the league teams list
+	 */
 	public void addTeams(Team team) {
 		teams.add(team);
 	}
-	public ArrayList getTeams () {
+	/**
+	 * 
+	 * @return list of teams
+	 */
+	public ArrayList<Team> getTeams () {
 		return teams;
 	}
 
